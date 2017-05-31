bc8c0f7f09a3a730176d0074dd273865c8addc85
choihonghee <vvshinevv@gmail.com>
2017-05-31 16:45:31 +0900

    [2017.05.31] 피보나치 수열 1) 변수 2개 2) 동적프로그래밍 3) 재귀 4) 재귀 개선

---
+++ ProgrammingInsight/[P55]피보나치 효율성.cpp
@@ -0,0 +1,50 @@
+#if 0
+#include <iostream>
+#define MAX 100
+using namespace std;
+
+long long memo[MAX];
+
+// 변수 2개 이용
+long long Fibo1(int n) {
+	long long update, save;
+	
+	if (n == 1 || n == 2) return 1;
+	save = update = 1;
+	for (int i = 3; i <= n; i++) {
+		update += save;
+		save = update - save;
+	}
+	return update;
+}
+
+// 배열 이용
+long long Fibo2(int n) {
+	
+	if (n == 1 || n == 2) return 1;
+	
+	memo[1] = memo[2] = 1;
+	for (int i = 3; i <= n; i++) 
+		memo[i] = memo[i - 1] + memo[i - 2];
+	
+	return memo[n];
+}
+
+// 가장 비효율적
+long long Fibo3(int n) {
+	if (n == 1 || n==2) return 1;
+	else return Fibo3(n - 1) + Fibo3(n - 2);
+}
+
+// 재귀 함수 개선
+long long Fibo4(int n) {
+	if (memo[n] > 0) return memo[n];
+	if (n == 1 || n == 2) return memo[n] = 1;
+	else return memo[n] = Fibo4(n - 1) + Fibo4(n - 2);
+}
+
+int main(void) {
+	cout << Fibo4(9);
+	return 0;
+}
+#endif
